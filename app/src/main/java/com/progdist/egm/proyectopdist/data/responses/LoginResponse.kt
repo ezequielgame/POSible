48980773ae8cedaa599ee6d6ac30f40df6e8254c
chequegame <ezequiel.gmejia@gmail.com>
2022-04-28 20:15:22 -0500

    Added Authentication

--- app/src/main/java/com/progdist/egm/proyectopdist/data/responses/LoginResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.progdist.egm.proyectopdist.data.responses
-
-data class LoginResponse(
-    val user: User
-)
---
+++ app/src/main/java/com/progdist/egm/proyectopdist/data/responses/auth/LoginResponse.kt
@@ -0,0 +1,5 @@
+package com.progdist.egm.proyectopdist.data.responses.auth
+
+data class LoginResponse(
+    val user: User
+)
